ee906ef0ef5d8898c1843db11ccc9309adda1697
HassanCoulibaly <hassansouleymanecoulibaly@gmail.com>
2025-04-07 19:44:13 +0000

    adding Name, level, and semester in comments

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+/**
+ * FOUNGNIGUE SOULEYMANE HASSAN COULIBALY
+ * CSC 6360
+ * GEORGIA STATE UNIVERSITY
+ * SPRING 2025
+ * GRADUATE STUDENT
+ */
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
